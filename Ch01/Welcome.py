fd769ebe9481df320fe3674b30e2b6f10f36ae01
SHANE BRUBAKER <shanebrubaker55@gmail.com>
2025-05-24 14:11:20 -0700

    Adding the py version of the notebook from Jupytext

---
+++ Ch01/Welcome.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+# BioInformatics with Python Cookbook - Fourth Edition #
+
+# +
+# Welcome to the book! #
+# 1-1 Welcome #
+# -
+
+print("Welcome to the BioInformatics with Python Cookbook Fourth Edition!")
+
+# +
+# Install packages using Conda
+# -
+
+# ! conda install biopython==1.84 jupyterlab==4.3.0 matplotlib==3.9.2 numpy==2.1.0 pandas==2.2.3 scipy==1.14.1 
+
+# +
+# Install Jupytext
+# -
+
+# ! pip install jupytext
+
+# +
+## End of Notebook ##
