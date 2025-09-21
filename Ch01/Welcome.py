110549340edcd1ecd51b65e01465c614f4f76d4a
SHANE BRUBAKER <shanebrubaker55@gmail.com>
2025-09-20 17:18:51 -0700

    Adding -y in Ch01 notebook

--- Ch01/Welcome.py
+++ Ch01/Welcome.py
@@ -27,7 +27,7 @@ print("Welcome to the BioInformatics with Python Cookbook Fourth Edition!")
 # Install packages using Conda
 # -
 
-# ! conda install biopython==1.84 jupyterlab==4.3.0 matplotlib==3.9.2 numpy==2.1.0 pandas==2.2.3 scipy==1.14.1 
+# ! conda install -y biopython==1.84 jupyterlab==4.3.0 matplotlib==3.9.2 numpy==2.1.0 pandas==2.2.3 scipy==1.14.1 
 
 # +
 # Install Jupytext
